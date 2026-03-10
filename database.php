1c318ac6ae6be79aa28237a705d785a5e52bd9dd
Dinesh verma <vermadinesh9693@gmail.com>
2026-03-10 07:02:46 +0530

    update db connection

--- database.php
+++ database.php
@@ -1,21 +1,32 @@
 <?php
 
-
-
 $host = getenv("DB_HOST") ?: "localhost";
-$user = getenv("DB_USER") ?: "root";
-$pass = getenv("DB_PASS") ?: "";
-$db = getenv("DB_NAME") ?: "jacresults";
+$user = getenv("DB_USERNAME") ?: "root";
+$pass = getenv("DB_PASSWORD") ?: "";
+$db   = getenv("DB_DATABASE") ?: "jacresults";
 $port = getenv("DB_PORT") ?: 3306;
 
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+$conn = mysqli_init(); 
 
-if (!$conn) {
-    die("Connection Failed: " . mysqli_connect_error());
-}
+//  Enable SSL (required by TiDB Cloud) 
+ mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+); 
 
+// $conn= mysqli_connect($host, $user, $pass, $db, $port);
+// if (!$conn) {
+//     die("Connection Failed: " . mysqli_connect_error());
+// }
 
-
+// echo "Connected successfully";
 
 ?>
